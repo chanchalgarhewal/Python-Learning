3f8f79bccb03c45cee5aabf1d8d93e590576e246
chanchal <garhewalchanchal@gmail.com>
2026-06-03 21:38:48 +0530

    Day-1 type_casting.py

---
+++ Day-1/type_casting.py
@@ -0,0 +1,23 @@
+# String to int 
+a="10"
+a=int(a)
+print(a)
+print(type(a))
+
+# Float to int 
+b=5.9
+b=int(b)
+print(b)
+print(type(b))
+
+# Int to int 
+c=5
+c=float(c)
+print(c)
+print(type(c))
+
+# Int to String
+d=100
+d=str(d)
+print(d)
+print(type(d))
