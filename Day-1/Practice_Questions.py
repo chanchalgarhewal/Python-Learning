0218a9ee0b8b5859a655489b415f68be48896f38
chanchal <garhewalchanchal@gmail.com>
2026-06-05 16:39:53 +0530

    Day-1 Practice-Questions.py

---
+++ Day-1/Practice_Questions.py
@@ -0,0 +1,28 @@
+# write a program to input 2 numbers & print their sum.
+first =int(input("Enter first number:"))
+second =int(input("Enter second number:"))
+
+print("sum =",first + second)
+
+
+# write a program to input side of a square & print its area.
+side =float(input("Enter side of a square:"))
+
+print("Area of square =",side*side)
+
+
+
+# write a ptogram to input 2 floating point mnumbers & print their average.
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+
+print("Average =", (num1 + num2) / 2)
+
+
+
+#write a progaram to input 2 int numbers , a and b.
+# print True if a is greater than or equal to b. if not print False.
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print(a>=b)
+
