45f289032dc4640c2b7e4d760a29b7f20fff067a
chanchal <garhewalchanchal@gmail.com>
2026-06-05 15:48:33 +0530

    day-1 operators.py

---
+++ Day-1/operators.py
@@ -0,0 +1,14 @@
+a=5
+b=10
+sum=a+b
+multi=a*b
+divide=a/b
+modulus=a%b
+subtract=a-b
+
+print("Sum:", sum)
+print("Multiplication:", multi)
+print("Division:", divide)  
+print("Modulus:", modulus)
+print("Subtraction:", subtract)
+    
