b6f711602bbddab049afdc03b963c48e6c408ade
Chanchal Garhewal <garhewalchanchal@gmail.com>
2026-09-16 23:19:09 +0530

    Add positive, negative, and even/odd checks

---
+++ Day-3/Positivenegative.py
@@ -0,0 +1,15 @@
+num =int(input("Enter a number"))
+
+if num>0:
+    print("positive")
+
+elif num<0:
+    print("negative")
+
+else:
+    print("zero")
+
+if num%2==0:
+    print("even")
+else:
+    print("odd")
