e44b7d3f3b8dd7691012a13cd994f2ea39046844
Chanchal Garhewal <garhewalchanchal@gmail.com>
2026-09-17 22:58:29 +0530

    Implement basic login system
    Corrected variable name and fixed login condition.

---
+++ Day-4/Loginsystem.py
@@ -0,0 +1,10 @@
+username=input("Enter your name:")
+password=input("Enter your password:")
+
+usename="Chanchal"
+password=12345678
+
+if username==username and password==password:
+    print("login successfully")
+else:
+    print("invailed username")
