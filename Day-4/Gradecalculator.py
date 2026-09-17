d972a68539ac2ca752a674cdef186247233fd420
Chanchal Garhewal <garhewalchanchal@gmail.com>
2026-09-17 23:00:00 +0530

    Add grade calculator based on input marks

---
+++ Day-4/Gradecalculator.py
@@ -0,0 +1,13 @@
+num=int(input("Enter a marks"))
+if num>=90:
+    print("A")
+elif num>=89:
+    print("B")
+elif num>=79:
+    print("C")
+elif num>=69:
+    print("D")
+elif num>=59:
+    print("E")
+else:
+    print("F")
