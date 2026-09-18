c9f7deadb7ac5b0584ae198b5217663fb62888ff
Chanchal Garhewal <garhewalchanchal@gmail.com>
2026-09-18 23:51:38 +0530

    Add Rock Paper Scissors game functionality
    Implement basic Rock Paper Scissors game logic.

---
+++ Day-5/Rock Paper Scissors Game.py
@@ -0,0 +1,15 @@
+user=(input("Enter your choice :"))
+computer="rock"
+if computer == user:
+  print("It's a Draw!:")
+elif  user!= computer:
+    if user=="paper":
+        print("You lose:")
+    elif user=="seissor":
+        print("you win:")
+else:
+    print("invalid input please re enter the correct input")
+    
+
+
+
